d2fcf6d7249dfb5f8e4c7bedd160d02d0f6e0474
mspinard <mspinard@vulog.com>
2025-07-03 15:49:24 +0200

    (fix): removed unnecessary comment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -26,7 +26,6 @@ function App() {
         }
       >
         <Route path="/dashboard" element={<DashboardPage />} />
-        {/* Plus tard : <Route path="/items" element={<ItemList />} /> */}
       </Route>
     </Routes>
   );
