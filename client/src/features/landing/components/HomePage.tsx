f623555ce4b6c014bd4f71a3b6714d527828f887
mspinard <maxiime.spinard@gmail.com>
2025-07-16 10:44:31 +0200

    refactor(landing): migrate landing page to feature-based architecture

---
+++ client/src/features/landing/components/HomePage.tsx
@@ -0,0 +1,15 @@
+import HeroSection from './HeroSection';
+import FeatureCardsSection from './FeatureCardsSection';
+import BottomCtaSection from './BottomCtaSection';
+
+const HomePage = () => {
+  return (
+    <>
+      <HeroSection />
+      <FeatureCardsSection />
+      <BottomCtaSection />
+    </>
+  );
+};
+
+export default HomePage;
