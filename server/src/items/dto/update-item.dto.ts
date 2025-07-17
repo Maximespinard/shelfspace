276507bbea6306c64eb072b029cc6273d61fa1eb
mspinard <maxiime.spinard@gmail.com>
2025-07-17 11:42:53 +0200

    fix: remove image if needed when updating item

--- server/src/items/dto/update-item.dto.ts
+++ server/src/items/dto/update-item.dto.ts
@@ -1,4 +1,15 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateItemDto } from './create-item.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateItemDto extends PartialType(CreateItemDto) {}
+export class UpdateItemDto extends PartialType(CreateItemDto) {
+  @ApiProperty({
+    description: 'Flag to indicate if the image should be removed',
+    example: true,
+    required: false,
+  })
+  @IsOptional()
+  @IsBoolean()
+  removeImage?: boolean;
+}
